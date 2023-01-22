8c4f15078c1892e6e879ef3a106bed3abde217e5
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2023-01-22 02:48:06 +0200

    Create Palindrome Partitioning.cpp

---
+++ Medium/131. Palindrome Partitioning/Palindrome Partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    int n;
+    vector<vector<string>> res;
+    vector<string> cur;
+public:
+    vector<vector<string>> partition(string s) {
+        n = s.size();
+        solve(0, s);
+        return res;
+    }
+
+    void solve(int idx, string &s) {
+        if (idx == n) {
+            res.push_back(cur);
+            return;
+        }
+
+        string curPalindrome = "";
+        for (int i = idx; i < n; ++i) {
+            curPalindrome += s[i];
+
+            if (isPalindrome(curPalindrome)) {
+                cur.push_back(curPalindrome);
+                solve(i + 1, s);
+                cur.pop_back();
+            }
+        }
+    }
+
+    bool isPalindrome(string &str) {
+        int sz = str.size();
+        for (int i = 0; i < sz / 2; ++i)
+            if (str[i] != str[sz - i - 1])
+                return false;
+
+        return true;
+    }
+};
