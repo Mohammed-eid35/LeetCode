e547640a0ff0080134e5cf48a7781c02798febc9
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2023-01-27 03:32:13 +0200

    Create Concatenated Words.cpp

---
+++ Hard/472. Concatenated Words/Concatenated Words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    unordered_set<string> dictionary;
+    unordered_map<string, int> dp;
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        dictionary = unordered_set(words.begin(), words.end());
+
+        vector<string> res;
+        for (auto &word : words)
+            if (numOfConcatenatedWords(word) > 1)
+                res.push_back(word);
+
+        return res;
+    }
+
+    int numOfConcatenatedWords(string word) {
+        if (word.empty()) return 0;
+        if (dp.count(word)) return dp[word];
+
+        string curPrefix = "";
+        int sz = word.size(), maxNumOfWords = INT_MIN;
+        for (int i = 0; i < sz; ++i) {
+            curPrefix += word[i];
+            
+            if (dictionary.count(curPrefix))
+                maxNumOfWords = max(
+                    maxNumOfWords,
+                    1 + numOfConcatenatedWords(word.substr(i + 1))
+                );
+        }
+
+        return dp[word] = maxNumOfWords;
+    }
+};
