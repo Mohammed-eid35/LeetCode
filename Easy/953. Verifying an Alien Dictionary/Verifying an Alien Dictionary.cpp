894f948b3c7e8820bdb1006c83c771a8b0f0b531
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2023-02-02 03:05:23 +0200

    Create Verifying an Alien Dictionary.cpp

---
+++ Easy/953. Verifying an Alien Dictionary/Verifying an Alien Dictionary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> weight;
+        for (int i = 0; i < 26; ++i)
+            weight[order[i]] = i;
+        
+        int n = words.size();
+        for (int i = 1; i < n; ++i)
+            if (compare(words[i - 1], words[i], weight))
+                return false;
+        
+        return true;
+    }
+
+    // return true if w1 > w2
+    bool compare(string &w1, string &w2, unordered_map<char, int> &weight) {
+        int sz = min(w1.size(), w2.size());
+        for (int i = 0; i < sz; ++i)
+            if (weight[w1[i]] != weight[w2[i]])
+                return weight[w1[i]] > weight[w2[i]];
+        
+        return w1.size() > w2.size();
+    }
+};
