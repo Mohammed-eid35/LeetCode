8cf3a404f0011593e946d8076f0293d5cd2a385c
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2023-02-09 07:04:13 +0200

    Create 2306. Naming a Company.cpp

---
+++ Hard/2306. Naming a Company/2306. Naming a Company.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        vector<unordered_set<string>> groups(26);
+        for (auto &idea : ideas)
+            groups[idea[0] - 'a'].insert(idea.substr(1));
+        
+        long long numOfNames = 0;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = i + 1; j < 26; ++j) {
+                int numOfSuffixes = 0;
+                for (auto &idea : groups[i])
+                    numOfSuffixes += groups[j].count(idea);
+                
+                numOfNames += 2LL * (groups[i].size() - numOfSuffixes) * (groups[j].size() - numOfSuffixes);
+            }
+        }
+
+        return numOfNames;
+    }
+};
