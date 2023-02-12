2c6d37d1287d25e2ccf86703a6386082afcf1d29
Alien <71411379+Mohammed-eid35@users.noreply.github.com>
2023-02-12 04:52:14 +0200

    Create Minimum Fuel Cost to Report to the Capital.cpp

---
+++ Medium/2477. Minimum Fuel Cost to Report to the Capital/Minimum Fuel Cost to Report to the Capital.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    long long numOfLiters = 0;
+    vector<vector<int>> graph;
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+        int n = roads.size();
+        graph = vector<vector<int>> (n + 1);
+
+        for (auto &road : roads) {
+            graph[road[0]].push_back(road[1]);
+            graph[road[1]].push_back(road[0]);
+        }
+
+        DFS(0, -1, seats);
+
+        return numOfLiters;
+    }
+
+    int DFS(int node, int parent, int seats) {
+        int representatives = 1;
+
+        for (auto child : graph[node])
+            if (child != parent)
+                representatives += DFS(child, node, seats);
+        
+        if (node != 0)
+            numOfLiters += (representatives + seats - 1) / seats;
+        
+        return representatives;
+    }
+};
